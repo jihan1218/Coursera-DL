997e74d5698ac702514b141c3183825fa998e890
Jihan Kim <jihan1218@gmail.com>
2020-10-30 17:49:46 -0700

    dnn utils file

--- Building_DNN/dnn_utils.py
+++ Building_DNN/dnn_utils.py
@@ -1,4 +1,23 @@
 import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+
+def load_data():
+    train_dataset = h5py.File("datasets/train_catvnoncat.h5", "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])
+
+    test_dataset = h5py.File("datasets/test_catvnoncat.h5", "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])
+
+    classes = np.array(test_dataset["list_classes"][:])
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
 
 def sigmoid(Z):
@@ -60,12 +79,11 @@ def initialize_parameters_deep(layer_dims):
     parameters = {}
     L = len(layer_dims)
     for l in range(1, L):
-        parameters["W" + str(l)] = np.random.randn(layer_dims[l],
-                                                   layer_dims[l - 1]) * 0.01
+        parameters["W" + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
         parameters["b" + str(l)] = np.zeros((layer_dims[l], 1))
 
-        assert(parameters["W" + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
-        assert(parameters["b" + str(l)].shape == (layer_dims[l], 1))
+        assert (parameters["W" + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters["b" + str(l)].shape == (layer_dims[l], 1))
 
     return parameters
 
@@ -73,7 +91,7 @@ def initialize_parameters_deep(layer_dims):
 def linear_forward(A, W, b):
     Z = np.dot(W, A) + b
 
-    assert(Z.shape == (W.shape[0], W.shape[1]))
+    assert (Z.shape == (W.shape[0], W.shape[1]))
     cache = (A, W, b)
 
     return Z, cache
@@ -88,7 +106,7 @@ def linear_activation_forward(A_prev, W, b, activation):
         Z, linear_cache = linear_forward(A_prev, W, b)
         A, activation_cache = relu(Z)
 
-    assert(A.shape == (W.shape[0], W.shape[1]))
+    assert (A.shape == (W.shape[0], W.shape[1]))
     cache = (linear_cache, activation_cache)
 
     return A, cache
@@ -101,15 +119,14 @@ def L_model_forward(X, parameters):
 
     for l in range(1, L):
         A_prev = A
-        A, cache = linear_activation_forward(
-            A, parameters["W" + str(l)], parameters["b" + str(l)], activation="relu")
+        A, cache = linear_activation_forward(A_prev, parameters["W" + str(l)], parameters["b" + str(l)],
+                                             activation="relu")
         caches.append(cache)
 
-    AL, cache = linear_activation_forward(
-        A, parameters["W" + str(L)], parameters["b" + str(l)], activation="sigmoid")
+    AL, cache = linear_activation_forward(A, parameters["W" + str(L)], parameters["b" + str(L)], activation="sigmoid")
     caches.append(cache)
 
-    assert(AL.shape == (1, X.shape[1]))
+    assert (AL.shape == (1, X.shape[1]))
 
     return AL, caches
 
@@ -120,7 +137,7 @@ def compute_cost(AL, Y):
     cost = (-1 / m) * np.sum(Y * np.log(AL) + (1 - Y) * np.log(1 - AL))
     cost = np.squeeze(cost)
 
-    assert(cost.shape == ())
+    assert (cost.shape == ())
 
     return cost
 
@@ -133,8 +150,84 @@ def linear_backward(dZ, cache):
     db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
     dA_prev = np.dot(W.T, dZ)
 
-    assert(dA_prev.shape == A_prev.shape)
-    assert(dW.shape == W.shape)
-    assert(db.shape == b.shape)
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
 
     return dA_prev, dW, db
+
+
+def linear_activation_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    if activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+
+def L_model_backward(AL, Y, caches):
+    grads = {}
+    L = len(caches)
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)
+
+    dAL = - np.divide(Y, AL) + np.divide(1 - Y, 1 - AL)
+    cache = caches[L - 1]
+    grads["dA" + str(L - 1)], grads["dW" + str(L - 1)], grads["db" + str(L - 1)] = linear_activation_backward(dAL,
+                                                                                                              cache,
+                                                                                                              activation="sigmoid")
+
+    for l in reversed(range(L)):
+        cache = caches[l]
+        grads["dA" + str(l - 1)], grads["dW" + str(l - 1)], grads["db" + str(l - 1)] = linear_activation_backward(
+            grads["dA" + str(l)], cache, activation="relu")
+
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate):
+    L = len(parameters) // 2
+    for l in range(1, L):
+        parameters["W" + str(l)] = parameters["W" + str(l)] - learning_rate * grads["dW" + str(l)]
+        parameters["b" + str(l)] = parameters["b" + str(l)] - learning_rate * grads["db" + str(l)]
+
+    return parameters
+
+
+def predict(X, Y, parameters):
+    m = X.shape[1]
+    n = len(parameters) // 2
+    p = np.zeros((1, m))
+
+    Y_hat, caches = L_model_forward(X, parameters)
+
+    for i in range(0, Y_hat.shape[1]):
+        if Y_hat[0, i] > 0.5:
+            p[0, i] = 1
+        else:
+            p[0, i] = 0
+    print("Accuracy: " + str(np.sum((p == Y)) / m))
+
+    return p
+
+
+def print_mislabeled_images(classes, X, Y, p):
+    a = p + Y
+    mislabeled_indices = np.asarray(np.where(a == 1))
+    plt.rcParams["figure.figsize"] = (40.0, 40.0)
+    num_images = len(mislabeled_indices[0])
+    for i in range(num_images):
+        index = mislabeled_indices[1][i]
+
+        plt.subplot(2, num_images, i + 1)
+        plt.imshow(X[:, index].reshape(64, 64, 3), interpolation="nearest")
+        plt.axis("off")
+        plt.title(
+            "Prediction: " + classes[int(p[0, index])].decode("utf-8") + " \n Class: " + classes[y[0, index]].decode(
+                "utf-8"))
