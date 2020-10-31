598c74431666026dba43c8d629448e32c9d3c4af
Jihan Kim <jihan1218@gmail.com>
2020-10-30 18:52:08 -0700

    add two layer & l layer NN model

--- Building_DNN/dnn_utils.py
+++ Building_DNN/dnn_utils.py
@@ -200,6 +200,72 @@ def update_parameters(parameters, grads, learning_rate):
     return parameters
 
 
+def two_layer_model(X, Y, layer_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):
+    grads = {}
+    costs = []
+    m = X.shape[1]
+    n_x, n_h, n_y = layer_dims
+
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    for i in range(0, num_iterations):
+        A1, cache1 = linear_activation_forward(X, W1, b1, activation="relu")
+        A2, cache2 = linear_activation_forward(A1, W2, b2, activation="sigmoid")
+        cost = compute_cost(A2, Y)
+        dA2 = -np.divide(Y, A2) + np.divide(1 - Y, 1 - A2)
+        dA1, dW2, db2 = linear_activation_backward(dA2, cache2, activation="sigmoid")
+        dA0, dW1, db1 = linear_activation_backward(dA1, cache1, activation="relu")
+
+        grads["dW1"] = dW1
+        grads["db1"] = db1
+        grads["dW2"] = dW2
+        grads["db2"] = db2
+
+        parameters = update_parameters(parameters, grads, learning_rate)
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+
+    plt.plot(np.squeeze(costs))
+    plt.ylabel("cost")
+    plt.xlabel("iterations (per hundreds)")
+    plt.title("Learning rate: " + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
+def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):
+    costs = []
+    parameters = initialize_parameters_deep(layers_dims)
+    for i in range(0, num_iterations):
+        AL, caches = L_model_forward(X, parameters)
+        cost = compute_cost(AL, Y)
+        grads = L_model_backward(AL, Y, caches)
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+
+    plt.plot(np.squeeze(costs))
+    plt.ylabel("cost")
+    plt.xlabel("iterations (per hundreds)")
+    plt.title("Learning rate: " + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+
 def predict(X, Y, parameters):
     m = X.shape[1]
     n = len(parameters) // 2
